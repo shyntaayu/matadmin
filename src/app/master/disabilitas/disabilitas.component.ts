8416ce93f10bab2719593061e277bbbbf9feda49
shyntaayu <shynta.add@gmail.com>
2020-06-01 16:44:45 +0700

    + master disabilitas oke

---
+++ src/app/master/disabilitas/disabilitas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disabilitas',
+  templateUrl: './disabilitas.component.html',
+  styleUrls: ['./disabilitas.component.css']
+})
+export class DisabilitasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
